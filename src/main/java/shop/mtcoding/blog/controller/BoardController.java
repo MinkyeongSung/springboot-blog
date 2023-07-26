0df21a4ff7261de51230d3e02e20ad0ea09340fc
MinkyeongSung <135561730+MinkyeongSung@users.noreply.github.com>
2023-07-26 17:46:17 +0900

    화면 구현 완료

---
+++ src/main/java/shop/mtcoding/blog/controller/BoardController.java
@@ -0,0 +1,28 @@
+package shop.mtcoding.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping({ "/", "/board" })
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/logo")
+    public String logo() {
+        return "redirect:/";
+    }
+
+    @GetMapping("/board/saveForm")
+    public String saveForm() {
+        return "board/saveForm";
+    }
+
+    @GetMapping("/board/1")
+    public String detail() {
+        return "board/detail";
+    }
+}
