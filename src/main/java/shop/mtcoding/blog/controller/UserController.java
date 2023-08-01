7747600d0daed26050db3c442ecd0c5c0dd6af0f
MinkyeongSung <135561730+MinkyeongSung@users.noreply.github.com>
2023-08-01 09:39:23 +0900

    회원가입 완료

--- src/main/java/shop/mtcoding/blog/controller/UserController.java
+++ src/main/java/shop/mtcoding/blog/controller/UserController.java
@@ -1,25 +1,42 @@
 package shop.mtcoding.blog.controller;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import shop.mtcoding.blog.dto.JoinDTO;
+import shop.mtcoding.blog.dto.LoginDTO;
+import shop.mtcoding.blog.model.User;
 import shop.mtcoding.blog.repository.UserRepository;
 
 @Controller
 public class UserController {
 
     @Autowired
+
     private UserRepository userRepository;
+
+    @PostMapping("/login")
+    public String login(LoginDTO loginDTO) {
+        // validation check (유효성 검사)
+        if (loginDTO.getUsername() == null || loginDTO.getUsername().isEmpty()) {
+            return "error/ex50x";
+        }
+        if (loginDTO.getPassword() == null || loginDTO.getPassword().isEmpty()) {
+            return "error/ex50x";
+        }
+        // 핵심 기능
+        User user = userRepository.findByUsernameAndPassword(loginDTO);
+
+        if (user == null) {
+
+        } else {
+
+        }
+
+        return "error/ex50x";
+    }
 
     // ip주소 부여 : 10.1.1.1. -> 도메인을 사서 mtcoding.com
     // 포트로 결정(:8080) -> 10.1.1.1:8080
@@ -59,6 +76,11 @@ public class UserController {
         }
         if (joinDTO.getUsername() == null || joinDTO.getUsername().isEmpty()) {
             return "error/ex40x";
+        }
+        try {
+            userRepository.save(joinDTO);
+        } catch (Exception e) {
+            return "redirect:/50x";
         }
         return "redirect:/loginForm";
     }
